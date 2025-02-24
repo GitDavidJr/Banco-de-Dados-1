451a4bec77ad1d8f799a59e27605cb8a1c5e552e
David Júnior <davidjunior051204@gmail.com>
2025-02-24 17:42:13 -0300

    fim

--- Projeto/3° Unidade/DQL/JCV_DQL_CONSULTAS.sql
+++ Projeto/3° Unidade/DQL/JCV_DQL_CONSULTAS.sql
@@ -1,18 +1,37 @@
--- faturamento total
-select sum(pedido.valor_total) as faturamento from pedido;
+-- faturamento total no ano de 2025
+select sum(pedido.valor_total) as faturamento 
+from pedido
+where year(data_criacao) = 2025;
 
--- custo de mao de obra
-select sum(custo_mao_de_obra) as 'custo mao de obra' from produz;
+-- custo de mao de obra no ano de 2025
+select sum(custo_mao_de_obra) as 'custo mao de obra' 
+from produz
+join produto
+on produto.codigo = produz.fk_produto_codigo
+join pedido
+on pedido.numero_do_pedido = fk_pedido_numero_do_pedido
+where year(data_criacao) = 2025;
 
 -- custo materia prima
-select sum(valor_unit) as 'custo materia prima' from materia_prima;
+select sum(valor_unit) as 'custo materia prima' 
+from materia_prima
+join composicao
+on composicao.fk_materia_prima_cod_materia_prima = materia_prima.cod_materia_prima
+join produto
+on produto.codigo = composicao.fk_produto_codigo
+join pedido
+on pedido.numero_do_pedido = produto.fk_pedido_numero_do_pedido
+where year(data_criacao) = 2025;
 
 -- lucro total
 select sum(pedido.valor_total) - ((select sum(custo_mao_de_obra) from produz) + (select sum(valor_unit) from materia_prima)) as lucro
-from pedido;
+from pedido
+where year(data_criacao) = 2025;
 
 -- media de valor das vendas
-select avg(pedido.valor_total) as 'media de preco dos pedidos' from pedido;
+select avg(pedido.valor_total) as 'media de preco dos pedidos'
+from pedido
+where year(data_criacao) = 2025;
 
 -- vendas por funcionarios
 select funcionario.nome as 'vendedor', sum(pedido.valor_total) as 'total de venda'
@@ -25,7 +44,7 @@ order by 'total de venda' desc;
 -- vendas acima de 1000 reais
 select count(*) as 'vendas acima de 1000 reais'
 from pedido
-where valor_total > 1000;
+where valor_total > 1000 and year(data_criacao) = 2025;
 
 -- clientes que gastaram mais de 1000 reais
 select clientes.nome as cliente, count(pedido.numero_do_pedido) as pedidos
@@ -60,6 +79,16 @@ join composicao
 on composicao.fk_materia_prima_cod_materia_prima = materia_prima.cod_materia_prima
 group by materia_prima.nome
 order by qtd_disponivel;
+
+select * from produto;
+
+select p.nome as 'Precisa de Metalon' from produto p
+join composicao c
+on c.fk_produto_codigo = p.codigo
+join materia_prima m
+on m.cod_materia_prima = c.fk_materia_prima_cod_materia_prima
+where m.nome like '%metalon%'
+group by p.nome;
 
 
 
